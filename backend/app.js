2ea3d092cd8636bbfc1be62719ee21a7221e08e8
Elias Leguizamon <elias.leguizamon1997@gmail.com>
2021-05-27 11:54:16 -0300

    add .gitignore

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+      });
+    socket.on('chat message', msg => {
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
